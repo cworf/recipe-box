76d1f701078e5cd98fbe68ea3961dcfe9f5d8d8e
Colin Worf and Sailor Winkelman <sailor@>
2018-01-16 11:54:53 -0800

    Use NgFor directive to loop through ingredient and steps arrays and add items to html using template.

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,9 +4,19 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
   <h1>Recipe Box</h1>
-  <ul>
-    <li *ngFor="let currentRecipe of recipes">{{currentRecipe.title}}</li>
-  </ul>
+  <div>
+    <div *ngFor="let currentRecipe of recipes">
+      <h2>{{currentRecipe.title}}</h2>
+      <h3>Ingredients</h3>
+      <ul>
+        <li *ngFor="let ingredient of currentRecipe.ingredients">{{ingredient}}</li>
+      </ul>
+      <h3>Instructions</h3>
+      <ul>
+        <li *ngFor="let step of currentRecipe.steps">{{step}}</li>
+      </ul>
+    </div>
+  </div>
   `
 })
 
@@ -14,7 +24,7 @@ export class AppComponent {
   currentRecipe: string =  'Recipes';
   recipes: Recipe [] = [
     new Recipe('Spaghetti', ['Pasta', 'Olive Oil', 'Tomatoes', 'Garlic', 'Basil', 'Salt', 'Pepper', 'Parmesean'], ['Add tomatoes, garlic, salt and oil to saucepan.', 'Simmer until flavor develops', 'Salt water and bring to a boil', 'Add pasta to water and boil for 7-10 minutes. Test noodle at 7 minutes.', 'Strain pasta in a collander and return to pot.', 'Add sauce to pot and mix with pasta', 'Add fresh grated parmesean, and serve.']),
-    new Recipe('Fettucini', ['Pasta', 'Olive Oil', 'Tomatoes', 'Garlic', 'Basil', 'Salt', 'Pepper', 'Parmesean'], ['Add tomatoes, garlic, salt and oil to saucepan.', 'Simmer until flavor develops', 'Salt water and bring to a boil', 'Add pasta to water and boil for 7-10 minutes. Test noodle at 7 minutes.', 'Strain pasta in a collander and return to pot.', 'Add sauce to pot and mix with pasta', 'Add fresh grated parmesean, and serve.'])
+    new Recipe('Enchiladas', ['Corn Tortillas', 'Green Chiles', 'Tomatillos', 'Garlic', 'Cilantro', 'Salt', 'Pepper', 'Cheese', 'Ground Beef', 'Onions'], ['Add Tomatillos, oil and seasoning to a pan an bring to a boil.', 'Simmer until flavor develops', 'Salt water and bring to a boil', 'Brown ground beef and onions', 'Add sauce and tortillas to baking sheet.', 'Add cheese amd bake for 20 min at 350 degrees F', 'Add fresh cilantro, and serve.'])
 
   ];
 
@@ -23,16 +33,3 @@ export class AppComponent {
 class Recipe {
   constructor(public title: string, public ingredients: string[], public steps: string[]){ }
 }
-
-// class Recipe {
-//   constructor(public title: string, public ingredients: string, public instructions: string){
-//   }
-// }
-
-
-// new Step('Add tomatoes, garlic, salt and oil to saucepan. Simmer until flavor develops'),
-// new Step('Salt water and bring to a boil'),
-// new Step('Add pasta to water and boil for 7-10 minutes. Test noodle at 7 minutes.'),
-// new Step('Strain pasta in a collander and return to pot'),
-// new Step('Add sauce to pot and mix with pasta'),
-// new Step('Add fresh grated parmesean, and serve').
