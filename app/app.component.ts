bdef78a64a7e80d257d1d9d80e27b420e919e97a
Colin Worf and Sailor Winkelman <colin+sailor@>
2018-01-16 11:17:35 -0800

    Add ngFor repeater directive

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,21 +4,24 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
   <h1>Recipe Box</h1>
+  <ul>
+    <li *ngFor="let currentRecipe of recipes">{{currentRecipe.title}}</li>
+  </ul>
   `
 })
 
 export class AppComponent {
-  currentRecipe: string =  'Spaghetti';
-
+  currentRecipe: string =  'Recipes';
   recipes: Recipe [] = [
-
+    new Recipe('Spaghetti', ['Pasta', 'Olive Oil', 'Tomatoes', 'Garlic', 'Basil', 'Salt', 'Pepper', 'Parmesean'], ['Add tomatoes, garlic, salt and oil to saucepan.', 'Simmer until flavor develops', 'Salt water and bring to a boil', 'Add pasta to water and boil for 7-10 minutes. Test noodle at 7 minutes.', 'Strain pasta in a collander and return to pot.', 'Add sauce to pot and mix with pasta', 'Add fresh grated parmesean, and serve.']),
+    new Recipe('Fettucini', ['Pasta', 'Olive Oil', 'Tomatoes', 'Garlic', 'Basil', 'Salt', 'Pepper', 'Parmesean'], ['Add tomatoes, garlic, salt and oil to saucepan.', 'Simmer until flavor develops', 'Salt water and bring to a boil', 'Add pasta to water and boil for 7-10 minutes. Test noodle at 7 minutes.', 'Strain pasta in a collander and return to pot.', 'Add sauce to pot and mix with pasta', 'Add fresh grated parmesean, and serve.'])
 
   ];
+
 }
 
 class Recipe {
-  constructor(public title: string, public ingredients: string, public steps: string){
-  }
+  constructor(public title: string, public ingredients: string[], public steps: string[]){ }
 }
 
 // class Recipe {
--- app/app.module.ts
+++ app/app.module.ts
@@ -8,6 +8,4 @@ import { AppComponent }   from './app.component';
   bootstrap:    [ AppComponent ]
 })
 
-export class AppModule {
-
-}
+export class AppModule { }
